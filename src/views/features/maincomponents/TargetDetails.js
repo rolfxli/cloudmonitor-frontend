bfdd92e9ac1a9c85cc7034afeba5f7c12623c872
Rolf Li <rolf.x.li@gmail.com>
2020-12-29 22:38:51 -0500

    adding chart

--- src/views/features/maincomponents/TargetDetails.js
+++ src/views/features/maincomponents/TargetDetails.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { useHistory, useParams } from "react-router-dom";
+import { 
+  CChart,
+  CChartLine } from '@coreui/react-chartjs'
 import axios from 'axios'
 import cookie from "js-cookie";
 
@@ -15,17 +18,19 @@ const TargetDetails = () => {
     mostrecentstatus: "",
     mostrecenterror: "",
     requestheaders: "",
-    requestbody = "",
-    requesttype = ""
+    requestbody: "",
+    requesttype: ""
   }
-  const { projectid } = useParams();
+  const { targetid } = useParams();
   const hisory = useHistory();
 
   const [responseHistory, setResponseHistory] = useState([])
+  const [responses, setResponses] = useState([])
 
   useEffect(() => {
     const token = cookie.get('token')
-    var url = `http://127.0.0.1:5000/targets/responses/${projectid}?token=${token}`
+    //console.log(targetid)
+    var url = `http://127.0.0.1:5000/targets/responses/${targetid}?token=${token}`
 
     axios.all(
       [
@@ -34,14 +39,73 @@ const TargetDetails = () => {
     ).then(
       axios.spread((res) => {
         setResponseHistory(res.data)
+        console.log(res.data)
+        console.log(responseHistory)
+        filterResponseTimes()
+        //console.log(responses)
       })
     ).catch((err) => {
       console.log(err)
     })
   }, [])
 
+  // set the ping times
+  function filterResponseTimes() {
+    var responseCount = responseHistory.length;
+    console.log(responseHistory)
+    var res = [];
+    for (let i = 0; i < responseCount; ++i) {
+      //console.log(responseHistory[i]['responsetime'])
+      res.append(responseHistory[i]['responsetime']);
+    }
+    setResponses(res);
+  }
+
+  const options = {
+    // tooltips: {
+    //   enabled: false,
+    //   custom: customTooltips
+    // },
+    maintainAspectRatio: false
+  }
+
+  const responseTimes = {
+    labels: [],
+    datasets: [
+      {
+        label: 'Response Times',
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: responses,
+      },
+    ],
+  };
+
     return (
-      <>Page working</>
+      <>
+       <div className="col-md-6">
+        <h4>Response History</h4>
+        <div className="chart-wrapper">
+          <CChart type="line" datasets={responseTimes.datasets} options={options} />
+        </div>
+        <hr />
+      </div>
+      </>
     );
   };
 
