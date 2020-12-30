bc9a80c786a4d2336e67db3256a8731d32bcfa28
a52wu <a52wu@uwaterloo.ca>
2020-12-29 21:06:52 -0500

    added json library for editing request headers/body

--- src/views/features/childcomponents/AddTarget.js
+++ src/views/features/childcomponents/AddTarget.js
@@ -9,62 +9,61 @@ import {
   CSpinner,
   CInputGroup,
   CInputGroupAppend,
-    CLabel,
-    CSelect,
+  CLabel,
+  CSelect,
   CModal,
   CModalBody,
   CModalHeader,
 } from "@coreui/react";
 
 import axios from "axios";
+import JSONInput from "react-json-editor-ajrm";
 
-const AddTarget = ({visible}) => {
+const AddTarget = ({ visible }) => {
+  const initialnewtarget = {
+    link: "",
+    testtype: "",
+  };
 
-    const initialnewtarget = {
-        link: "",
-        testtype: ""
-    }
+  var requesttypes = [
+    { name: "GET", value: "GET" },
+    { name: "POST", value: "POST" },
+    { name: "PUT", value: "PUT" },
+    { name: "DELETE", value: "DELETE" },
+  ];
 
-    var requesttypes = [
-        {name: 'GET', value: 'GET'},
-        {name: 'POST', value: 'POST'},
-        {name: 'PUT', value: 'PUT'},
-        {name: 'DELETE', value: 'DELETE'},
-
-    ]
-
-    
-
-    const [newtarget, setNewtarget] = useState(initialnewtarget)
+  const [newtarget, setNewtarget] = useState(initialnewtarget);
 
   return (
-    <><CModal color='primary' show={visible}>
+    <>
+      <CModal color="primary" show={visible}>
         <CModalHeader>Add New Target</CModalHeader>
         <CModalBody>
-
-            <CLabel>
-                Url Link
-            </CLabel>
-            <CInput
+          <CLabel>Url Link</CLabel>
+          <CInput
             type="text"
             name="link"
             value={newtarget.link}
             placeholder="Eg. https://reqres.in/api/products/4"
-            />
+          />
 
+          <CLabel>Request Type</CLabel>
+          <CSelect type="select" name="requesttype">
+            {requesttypes.map((option) => (
+              <option value={option.value}>{option.name}</option>
+            ))}
+          </CSelect>
 
-            <CLabel>Request Type</CLabel>
-                          <CSelect
-                            type="select"                            
-                            name="requesttype"
-                          >
-                            {requesttypes.map((option) => <option value={option.value}>{option.name}</option>)}
-                            
-                          </CSelect>
-                          
-            <CButton color='success'>Create</CButton>
+          <CLabel>Headers (Enter a valid JSON object)</CLabel>
+          <JSONInput id="a_unique_id" height="250px" />
+
+          <CLabel>Request Body (Enter a valid JSON object)</CLabel>
+          <JSONInput id="a_unique_id" height="250px" />
+
+          <CButton color="success">Create</CButton>
         </CModalBody>
-        </CModal></>
+      </CModal>
+    </>
   );
 };
 
